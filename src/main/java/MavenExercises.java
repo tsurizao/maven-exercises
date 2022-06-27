94c180d36fdcf0a3ff853e734041d88e8e66856f
Chase Martinez <chasemartinez22@gmail.com>
2022-06-27 13:43:49 -0500

    Refactored swapCase and reverse variables to remove unnecessary code

--- src/main/java/MavenExercises.java
+++ src/main/java/MavenExercises.java
@@ -1,4 +1,5 @@
 import org.apache.commons.lang3.StringUtils;
+
 import java.util.Scanner;
 
 public class MavenExercises {
@@ -7,15 +8,13 @@ public class MavenExercises {
         System.out.println("Enter something: ");
         String userInput = sc.nextLine();
 
-        if(StringUtils.isNumeric(userInput)){
+        if (StringUtils.isNumeric(userInput)) {
             System.out.println(userInput + " is a number.");
         } else {
             System.out.println(userInput + " is not a number.");
         }
-        userInput = StringUtils.swapCase(userInput);
-        System.out.println("Flipping the case: " + userInput);
 
-        userInput = StringUtils.reverse(userInput);
-        System.out.println("Reversed: " + userInput);
+        System.out.println("Flipping the case: " + StringUtils.swapCase(userInput));
+        System.out.println("Reversed: " + StringUtils.reverse(userInput));
     }
 }
